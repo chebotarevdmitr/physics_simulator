a181b7061bcb850faa715ea62305b7db17f375c6
Dmitriy Chebotarev <4ebotarov_dim@inbox.ru>
2025-03-04 15:12:57 +0300

    modified src/World.cpp

--- src/World.cpp
+++ src/World.cpp
@@ -14,6 +14,15 @@ World::World(const std::string& filePath) {
 
     std::string line;
     while (std::getline(file, line)) {
+        // Удаляем пробелы в начале и конце строки
+        line.erase(0, line.find_first_not_of(" \t"));
+        line.erase(line.find_last_not_of(" \t") + 1);
+
+        // Игнорируем пустые строки и комментарии
+        if (line.empty() || line[0] == '#') {
+            continue;
+        }
+
         std::istringstream iss(line);
         if (iss >> x >> y >> vx >> vy >> red >> green >> blue >> radius >> std::boolalpha >> isCollidable) {
             Point center(x, y);
